3d34ae12212d24740cd0e95c154f95ae233429ff
Grrravity <julienne.prof@gmail.com>
2018-07-02 12:14:56 +0200

    Corrected : MainActivity > saveMood() Method now works with right storedmoods. Added begining of Recicler view for history activity.

---
+++ app/src/main/java/com/happyapp/grrravity/moodtracker/controller/adapter/HistoryAdapter.java
@@ -0,0 +1,13 @@
+package com.happyapp.grrravity.moodtracker.controller.adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_adapter);
+    }
+}
